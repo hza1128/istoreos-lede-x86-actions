489f0ca1968f20bb24650b95ab3ffd5bdbeb0680
hza1128 <141570215+hza1128@users.noreply.github.com>
2025-12-15 13:00:40 +0800

    Update versioning in default-settings file

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,7 +38,7 @@ sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/file
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by EircoD/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/${orig_version}/R${date_version} by hza800755/g" package/lean/default-settings/files/zzz-default-settings
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
